d052a00ae228b7192e1b00725e5ad41f726318dd
Jakub Roks <kubaroks@onet.eu>
2024-08-17 22:39:44 +0200

    RunningSumof1dArray1480

---
+++ src/main/java/org/example/Challenges/FindWordsContainingCharacter2942/FindWordsContainingCharacter2942.java
@@ -0,0 +1,15 @@
+package org.example.Challenges.FindWordsContainingCharacter2942;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindWordsContainingCharacter2942 {
+//    public List<Integer> findWordsContaining(String[] words, char x) {
+//        List<Integer> res = new ArrayList();
+//
+//        for(int i = 0; i < words.length; i++) {
+//
+//        }
+//
+//    }
+}
